060a9d631e738c3544907e786c4e6ec06a0e1e6b
Kevin Iturralde <keviniturralde3@gmail.com>
2020-07-13 23:51:01 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,7 +3,7 @@
 def start_game (player1, player2)
   puts "Hello #{player1} & #{player2}"
 end
-start_game ("Rocky", "Bullwinkle")
+start_game ("Rocky Bullwinkle"
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
