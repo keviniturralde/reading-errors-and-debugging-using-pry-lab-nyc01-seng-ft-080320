f5d8f132c3c503c23fd1d9bd57a87bfe88af47fd
Kevin Iturralde <keviniturralde3@gmail.com>
2020-07-13 23:37:01 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,7 +3,7 @@ require 'pry'
 def start_game(player1, player2)
   puts "Hello #{player1} & #{player2}"
 end
-start_game)
+start_game
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
