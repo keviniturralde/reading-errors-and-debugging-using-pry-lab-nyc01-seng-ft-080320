a41a8e3991a894fec79d0e858954574b95942286
Kevin Iturralde <keviniturralde3@gmail.com>
2020-07-13 23:47:22 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,10 @@
 # don't forget to add: require 'pry'
 require 'pry'
-def start_game (player1 = "Rocky", player2 = "Bullwinkle")
+def start_game (player1, player2)
   puts "Hello #{player1} & #{player2}"
 end
 puts start_game (player1, player2)
+
 def play_game(player2)
   "#{player1} is better than #{player2}"
 end
