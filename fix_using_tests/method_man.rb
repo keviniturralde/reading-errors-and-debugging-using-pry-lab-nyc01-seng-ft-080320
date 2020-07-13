5db4d86fb42cdb090d9c301378b90049ae5e6704
Kevin Iturralde <keviniturralde3@gmail.com>
2020-07-13 23:39:25 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,7 +4,7 @@ def start_game(player1, player2)
   puts "Hello #{player1} & #{player2}"
 end
 start_game ("Rocky", "Bullwinkle")
-
+binding.pry
 def play_game(player2)
   "#{player1} is better than #{player2}"
 end
